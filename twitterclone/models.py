ab957180ee77cbac09ed0102baad556172a8cff3
unknown <shomok0106k@gmail.com>
2020-07-12 11:18:20 +0900

    user-login-signup

---
+++ twitterclone/models.py
@@ -0,0 +1,21 @@
+import datetime
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+    def was_published_recently(self):
+        return self.published_date >= timezone.now() - datetime.timedelta(days=1)
