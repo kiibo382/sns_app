331726feed66f51cc1aacd8f3f8361fadc6a7f26
unknown <shomok0106k@gmail.com>
2020-08-03 18:06:43 +0900

    fix

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from . import forms
 from django.shortcuts import render,  get_object_or_404
 from friendship.models import Friend, Follow, Block, FriendshipRequest
 import pdb
+from django.views.decorators.http import require_POST, require_GET
 
 class Login(LoginView):
     form_class = forms.LoginForm
@@ -25,51 +26,51 @@ class Profile(TemplateView):
     template_name = "accounts/profile.html"
 
     def get_context_data(self, **kwargs):
-        user = get_object_or_404(User, id=self.kwargs['pk'])
-        login_user = self.request.user
-        posts = user.post_set.all().order_by('-published_date')
-        friends = Friend.objects.are_friends(login_user, user)
-        following = Follow.objects.following(login_user)
-        blocking_user = Block.objects.is_blocked(login_user, user)
-        like_posts = user.like_user.all()
+        context = super().get_context_data(**kwargs)
+        display_user = get_object_or_404(User, id=self.kwargs['pk'])
+        posts = display_user.post_set.all().order_by('-published_date')
+        like_posts = display_user.like_user.all()
         try:
-            requesting_follows = FriendshipRequest.objects.get(to_user=user.id, from_user=login_user.id)
+            login_user = self.request.user
+            requesting_follows = FriendshipRequest.objects.get(to_user=display_user.id, from_user=login_user.id)
+            friends = Friend.objects.are_friends(login_user, display_user)
+            following = Follow.objects.following(login_user)
+            blocking_user = Block.objects.is_blocked(login_user, display_user)
         except:
             pass
 
-        context = {'user': user,
-                   'login_user': login_user,
-                   'posts': posts,
-                   'friends': friends,
-                   'following': following,
-                   'blocking_user': blocking_user,
-                   'like_posts': like_posts,
-                   }
+        context['display_user']=display_user
+        context['posts']=posts
+        context['like_posts']=like_posts
         try:
             context['requesting_follows'] = requesting_follows
+            context['login_user']= login_user
+            context['friends']=friends
+            context['following']=following
+            context['blocking_user']=blocking_user
         except:
             pass
         # pdb.set_trace()
         return context
 
+@require_POST
 def follow(request, pk):
     user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        if 'request_follow' in request.POST:
-           Friend.objects.add_friend(request.user, user)
-        if 'remove_follow' in request.POST:
-            Friend.objects.remove_friend(request.user, user)
-        if 'not_follow' in request.POST:
-            Friend.objects.remove_friend(request.user, user)
+    if 'request_follow' in request.POST:
+       Friend.objects.add_friend(request.user, user)
+    if 'remove_follow' in request.POST:
+        Friend.objects.remove_friend(request.user, user)
+    if 'not_follow' in request.POST:
+        Friend.objects.remove_friend(request.user, user)
     return render(request, 'accounts/follow.html', {'login_user': request.user})
 
+@require_POST
 def block(request, pk):
     other_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        if 'block' in request.POST:
-           Block.objects.add_block(request.user, other_user)
-        if 'remove_block' in request.POST:
-           Block.objects.remove_block(request.user, other_user)
+    if 'block' in request.POST:
+       Block.objects.add_block(request.user, other_user)
+    if 'remove_block' in request.POST:
+       Block.objects.remove_block(request.user, other_user)
     return render(request, 'accounts/block.html',{'login_user': request.user})
 
 
@@ -97,24 +98,23 @@ class Info(TemplateView):
         }
         return context
 
+@require_POST
 def accept(request, pk):
     user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        if 'accept' in request.POST:
-            # pdb.set_trace()
-            friend_request = FriendshipRequest.objects.get(from_user=request.user, to_user=pk)
-            friend_request.accept()
-            Follow.objects.add_follower(request.user, user)
-        return render(request, 'accounts/accept.html',{'login_user': request.user})
+    # if 'accept' in request.POST:
+        # pdb.set_trace()
+    friend_request = FriendshipRequest.objects.get(from_user=pk, to_user=request.user.id)
+    friend_request.accept()
+    Follow.objects.add_follower(request.user, user)
+    return render(request, 'accounts/accept.html',{'login_user': request.user})
 
+@require_POST
 def reject(request, pk):
     user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        if 'reject' in request.POST:
-            friend_request = FriendshipRequest.objects.get(from_user=request.user,to_user=pk)
-            friend_request.reject()
-            Friend.objects.remove_friend(request.user, user)
-        return render(request, 'accounts/reject.html', {'login_user': request.user})
+    friend_request = FriendshipRequest.objects.get(from_user=pk, to_user=request.user.id)
+    friend_request.reject()
+    Friend.objects.remove_friend(request.user, user)
+    return render(request, 'accounts/reject.html', {'login_user': request.user})
 
 class IndexView(ListView):
     model = User
